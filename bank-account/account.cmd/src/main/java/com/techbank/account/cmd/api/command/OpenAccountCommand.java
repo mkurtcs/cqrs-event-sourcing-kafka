fccb39ee4ae886edc632531def037c7aa4b66ea9
mkurt <info.mkurt@gmail.com>
2022-01-07 23:22:48 +0300

    Create command classes. closes #5.

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/command/OpenAccountCommand.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/command/OpenAccountCommand.java
@@ -9,5 +9,5 @@ public class OpenAccountCommand extends BaseCommand {
 
     private String accountHolder;
     private AccountType accountType;
-    private double balance;
+    private double openingBalance;
 }
--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/command/BaseCommand.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/command/BaseCommand.java
@@ -2,10 +2,12 @@ package com.techbank.cqrs.core.command;
 
 import com.techbank.cqrs.core.message.Message;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public abstract class BaseCommand extends Message {
 
     public BaseCommand(String id) {
